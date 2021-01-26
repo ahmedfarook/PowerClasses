7e6425dce8956bc4338e079b940e0f19c7e4d86c
Ahmed Farook <ahmedgaziyani@gmail.com>
2021-01-26 20:59:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function random()
+{
+    var num = (Math.floor(Math.random() * Math.floor(6)))
+    return num
+    
+}
+
+    document.getElementById('image1').src = "image/azurecloud/" + random() + ".jpg"
+    
+    
+
+
+//document.getElementsByClassName().innerhtml
+
+//="images/" + random() + ".jpg"
+//console.log(x)
